81243b80af747e93f2d0a940a8c1357243cf9935
Kory Campbell <korycampbell@Korys-MacBook-Pro.local>
2021-03-22 20:34:19 -0400

    added axios call to update count on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.scss';
 
 function App() {
+
+  const [data, setData] = useState({ value: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        "https://api.countapi.xyz/update/1ccb732e-b55a-4404-ad3f-0f99c02fe44e/?amount=1",
+      );
+ 
+      setData(result.data);
+      console.log(result.data);
+    };
+
+    fetchData();
+  }, []);
+
+  //update count on button click
+  const updateCount = async () => {
+
+    const result = await axios(
+      "https://api.countapi.xyz/update/1ccb732e-b55a-4404-ad3f-0f99c02fe44e/?amount=1",
+    );
+
+    setData(result.data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="header-text">
+        <h1>Project Purple Cow 🐮</h1>
+      </div>
       </header>
+      <div>
+
+ {/* Page Count Header */}
+<h1>This page has {data.value} views.</h1>
+
+
+ {/* Update Count Button */}
+<div>
+  <button className="bg-primary button-class" onClick={updateCount}>
+    <i className="fas fa-home"></i> Update Count </button>
+</div>
+      </div>
+
     </div>
   );
 }
 
+
+
 export default App;
+
+
